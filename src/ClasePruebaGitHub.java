35a2e8cc0c747f236196298447a5d4c7a4db1585
JavierBailen <javier.bailen@fernando3martos.com>
2023-11-07 13:38:08 +0100

    segundo cambio desde intellij

--- src/ClasePruebaGitHub.java
+++ src/ClasePruebaGitHub.java
@@ -2,5 +2,6 @@ public class ClasePruebaGitHub {
     public static void main(String[] args) {
         System.out.println("Hola colaborador, que te cuentas?");
         System.out.println("aaaaa");
+        System.out.println("Otro cambio");
     }
 }
